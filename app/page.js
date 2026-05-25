4f7c93cd0feeb45fd756e04dddeea5b18b8e105c
Rickzz y <duttarick6203@gmail.com>
2026-05-25 13:47:14 +0530

    Removed the unused async getData()

--- app/page.js
+++ app/page.js
@@ -1,4 +1,3 @@
-import { personalData } from "@/utils/data/personal-data";
 import AboutSection from "./components/homepage/about";
 import Education from "./components/homepage/education";
 import Experience from "./components/homepage/experience";
@@ -16,24 +15,7 @@ const ContactSection = dynamic(
   { ssr: false }
 );
 
-
-async function getData() {
-  const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`)
-
-  if (!res.ok) {
-    throw new Error('Failed to fetch data')
-  }
-
-  const data = await res.json();
-
-  const filtered = data.filter((item) => item?.cover_image).sort(() => Math.random() - 0.5);
-
-  return filtered;
-};
-
-export default async function Home() {
-  const blogs = await getData();
-
+export default function Home() {
   return (
     <>
       <HeroSection />
